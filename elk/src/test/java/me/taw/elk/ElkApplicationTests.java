1e2deb42ddff57cce0f401afdd579f4b3536642f
aw <1541998@qq.com>
2022-09-10 07:01:22 +0800

    elk test

---
+++ elk/src/test/java/me/taw/elk/ElkApplicationTests.java
@@ -0,0 +1,13 @@
+package me.taw.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
